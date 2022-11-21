a963132e66d9e170ea6f566a76b42c461a542e84
victor.govorov <victorgovorov@mail.ru>
2022-11-21 12:24:57 +0500

    removed MaskedJsonTypeInfoResolver

--- src/Byndyusoft.MaskedSerialization/Helpers/MaskedSerializationHelper.cs
+++ src/Byndyusoft.MaskedSerialization/Helpers/MaskedSerializationHelper.cs
@@ -1,16 +1,37 @@
 ﻿namespace Byndyusoft.MaskedSerialization.Helpers
 {
     using System.Text.Json;
-    using System.Text.Json.Serialization;
+    using System.Text.Json.Serialization.Metadata;
+    using Annotations.Attributes;
+    using Core.MaskingInfo;
     using Converters;
 
     public static class MaskedSerializationHelper
     {
-        public static JsonConverter MaskedConverterFactory { get; } = new MaskedConverterFactory();
-
         public static void SetupOptionsForMaskedSerialization(JsonSerializerOptions options)
         {
-            options.TypeInfoResolver = new MaskedJsonTypeInfoResolver();
+            var resolver =
+                (DefaultJsonTypeInfoResolver)(options.TypeInfoResolver ??= new DefaultJsonTypeInfoResolver());
+            resolver.Modifiers.Add(DetectMaskedMemberAttribute);
+        }
+
+        private static void DetectMaskedMemberAttribute(JsonTypeInfo typeInfo)
+        {
+            if (typeInfo.Kind != JsonTypeInfoKind.Object)
+                return;
+
+            var typeMaskingInfo = TypeMaskingInfoHelper.Get(typeInfo.Type);
+            if (typeMaskingInfo.HasMaskedProperties == false)
+                return;
+
+            foreach (var propertyInfo in typeInfo.Properties)
+            {
+                if (propertyInfo.AttributeProvider is { } provider &&
+                    provider.IsDefined(typeof(MaskedAttribute), inherit: true))
+                {
+                    propertyInfo.CustomConverter = new MaskedConverterFactory();
+                }
+            }
         }
 
         public static JsonSerializerOptions GetOptionsForMaskedSerialization()
